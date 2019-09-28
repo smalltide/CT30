983ba0415baff5971953fbba15dfd8963dfb1fe4
ice <smalltides@gmail.com>
2019-09-28 23:58:19 +0800

    add HelloWorld.js test step comment

--- day12/HelloWorld.js
+++ day12/HelloWorld.js
@@ -1,13 +1,12 @@
-import { Selector } from 'testcafe';
+import { Selector } from 'testcafe'; // 引入 testcafe 的 html element 選擇器
 
-fixture `Getting Started`
-    .page `http://devexpress.github.io/testcafe/example`;
+fixture `Getting Started` // 設定測試集名稱
+    .page `http://devexpress.github.io/testcafe/example`; // 測試目標網頁
 
-test('HelloWorld test', async t => {
+test('HelloWorld test', async t => { // 一個測試案例
     await t
-        .typeText('#developer-name', 'HelloWorld')
-        .click('#submit-button')
-
-        // Use the assertion to check if the actual header text is equal to the expected one
+        .typeText('#developer-name', 'HelloWorld') //在文字框輸入 HelloWorld
+        .click('#submit-button') //按下送出按鈕
         .expect(Selector('#article-header').innerText).eql('Thank you, HelloWorld!');
+        // 使用 expect 驗證 #article-header 內的文字 等於 Thank you, HelloWorld!
 });
